f88d398dc0ae443324568b3ffe8c845f1eee254e
Edvaldo <edvaldovitor250@gmail.com>
2023-12-19 14:00:44 -0300

    first commit

---
+++ Conexao.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroProjeto
+{
+    using Microsoft.Xrm.Tooling.Connector;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace MeuPrimeiroProjeto
+    {
+        internal class Conexao
+        {
+            private static CrmServiceClient crmServiceClient;
+
+
+
+        }
+    }
+
+}
